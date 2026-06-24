aceda811bff840f8648181b1cdcfb6f020c6b808
Abdul Samad <iamabdulsamad2.0@gmail.com>
2026-06-24 17:57:34 +0500

    fix the pnpm config error

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -46,7 +46,7 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 
 app.listen(PORT, () => {
   console.log(`Server is running at this PORT : ${PORT}`);
